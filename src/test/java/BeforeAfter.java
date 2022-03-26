1b04576ddf697a25775fc324ced331c77feca396
DudjakPeter <peter.dudjak@gmail.com>
2022-03-26 08:21:57 +0100

    Update BeforeAfter.java

--- src/test/java/BeforeAfter.java
+++ src/test/java/BeforeAfter.java
@@ -1,4 +1,5 @@
 import io.restassured.*;
+import org.codehaus.groovy.reflection.*;
 import org.junit.jupiter.api.*;
 
 import java.util.*;
@@ -11,23 +12,22 @@ public class BeforeAfter {
     int randomCount = new Random().nextInt((52 - 1 )+1)+1;
 
     @BeforeAll
-    public static void setup() {
+    public static void beforeAllSteps() {
         RestAssured.baseURI = "https://deckofcardsapi.com";
         RestAssured.basePath = "/api/deck";
     }
 
     @BeforeEach
-    public void init (TestInfo testInfo) {
+    public void beforeEachSteps (TestInfo testInfo) {
         System.out.println("Running test: " +testInfo.getDisplayName());
     }
 
     @AfterAll
-    public static void cleanup () {
-
+    public static void afterAllSteps (TestInfo testInfo) {
+        System.out.println("Finished testing of suite: "+ testInfo.getDisplayName());
     }
 
     @AfterEach
-    public void teardown () {
-        System.out.println("Finished testing of suite: "+ this.getClass().getSimpleName());
+    public void afterEachSteps () {
     }
 }
